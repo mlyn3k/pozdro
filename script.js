fa4c1364bfe393a9a9dce785ac0bd7cbe21276bc
mlyn3k <116730546+mlyn3k@users.noreply.github.com>
2022-10-29 15:12:11 +0200

    Add by website

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict"
+let yourFirstName = prompt ("Jak masz na imię", "Twoje imię");
+alert(`Cześć ${yourFirstName}, dobrze że jesteś!`);
+let yourAge = prompt ("Ile masz lat?", "Twój wiek");
+alert (`${yourFirstName}, wyglądasz bardzo młodo mimo ${yourAge} lat! Pozdrawiam - Łukasz Młynarczyk`);
